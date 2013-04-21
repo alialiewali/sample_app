89a95db9d78e203178cfcada2118d8e4be5f85dc
alialiewali <alialiewali@mail.ru>
2013-04-21 14:02:48 +0400

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
-
+gem 'bootstrap-sass', '2.1'
 group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.11.0'
@@ -31,3 +31,4 @@ end
 group :production do
   gem 'pg', '0.12.2'
 end
+# Специальная установка в случае появления ошибок bundle install --path .bundle
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,6 +37,9 @@ Spork.prefork do
     # automatically. This will be the default behavior in future versions of
     # rspec-rails.
     config.infer_base_class_for_anonymous_controllers = false
+
+# added after error from here http://stackoverflow.com/questions/9475857/rspec-and-named-routes
+    config.include Rails.application.routes.url_helpers
   end
 end
 
